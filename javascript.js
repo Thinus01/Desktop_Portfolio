3d8d2612e627f097b353e6764f501935d409a72e
Thinus <thinusvdv2001@gmail.com>
2022-11-21 16:09:52 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,35 +1,28 @@
-const hamburger = document.querySelector(".hamburger");
-const ham_btn = document.querySelector(".ham_btn");
-const trans_lines = document.querySelector(".trans_lines");
-const clicked = document.querySelector(".clicked");
-const pText = document.querySelector("#primary_text");
-const btns = document.querySelectorAll(".btn");
+const hamburger = document.querySelector('.hamburger');
+const hamBtn = document.querySelector('.ham_btn');
+const transLines = document.querySelector('.trans_lines');
+const clicked = document.querySelector('.clicked');
+const pText = document.querySelector('#primary_text');
+const btns = document.querySelectorAll('.btn');
 
 btns.forEach(btn => {
-        btn.addEventListener("click", () => {
-        hamburger.classList.toggle("hidden");
-        trans_lines.classList.toggle("hidden");
-        clicked.classList.toggle("hidden");
-        pText.classList.toggle("hidden");
-        
-        for(let i = 1; i <= 3; i++){
-            trans_lines.classList.toggle("line_rotate" + i.toString());
-        }
-    
-    })
+    btn.addEventListener('click', () => {
+    hamburger.classList.toggle('hidden');
+    transLines.classList.toggle('hidden');
+    clicked.classList.toggle('hidden');
+    pText.classList.toggle('hidden');
+    for (let i = 1; i <= 3; i+=1){
+        transLines.classList.toggle('line_rotate' + i.toString());
+    }
+  });
 });
 
-
-ham_btn.addEventListener("click", () => {
-    hamburger.classList.toggle("hidden");
-    trans_lines.classList.toggle("hidden");
-    clicked.classList.toggle("hidden");
-    pText.classList.toggle("hidden");
-
-    for(let i = 1; i <= 3; i++){
-        trans_lines.classList.toggle("line_rotate" + i.toString());
-    }
-
-})
-
-//CHECKPOINT .............................................................
+hamBtn.addEventListener('click', () => {
+  hamburger.classList.toggle('hidden');
+  transLines.classList.toggle('hidden');
+  clicked.classList.toggle('hidden');
+  pText.classList.toggle('hidden');
+  for (let i = 1; i <= 3; i+=1){
+    transLines.classList.toggle('line_rotate' + i.toString());
+  }
+});
