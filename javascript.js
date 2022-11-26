29a97c6933975d2a7734df3c229b2c45a906daf1
Thinus <thinusvdv2001@gmail.com>
2022-11-26 04:39:03 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,8 +10,6 @@ const body = document.querySelector('body');
 const section = document.createElement('section');
 body.appendChild(section);
 const form = document.getElementById('get_in_touch_button');
-const forms = document.getElementById('get_in_touch_buttons');
-const errorM = document.getElementById('Error');
 
 section.innerHTML = `
 <section id="list_portfolio">
@@ -379,21 +377,16 @@ for (let i = 0; i < buttons.length; i += 1) {
   });
 }
 
-
 function onSubmit(e) {
-    const userEmail = document.getElementById('user_email');
-    const email = userEmail.value;
+  const userEmail = document.getElementById('user_email');
+  const email = userEmail.value;
 
-    if(email !== email.toLocaleLowerCase()){
-        e.preventDefault();
-        errorM.style.display = 'flex';
-        console.log('more');
-        console.log(email)
-    } else {
-        errorM.style.display = 'none';
-        console.log('good')
-        console.log(email)
-    }
+  if (email !== email.toLocaleLowerCase()) {
+    e.preventDefault();
+    errorM.style.display = 'flex';
+  } else {
+    errorM.style.display = 'none';
+  };
 }
 
 const subLink = document.getElementById('submitLink');
